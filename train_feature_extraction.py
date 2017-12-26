0873190005778e5833255ebe8c2f77d992252f95
abang <shenzb12@lzu.edu.cn>
2017-12-26 11:16:25 +0800

    use the alnex net to deal with the cifar-10

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -6,63 +6,63 @@ from sklearn.utils import shuffle
 import time
 
 nb_classes = 43
-batch_size = 64
-epochs = 20
+batch_size = 128
+epochs = 10
 
-# Load traffic signs data.
-with open('train.p', 'rb') as f:
-    data = pickle.load(f)
+# # Load traffic signs data.
+# with open('train.p', 'rb') as f:
+#     data = pickle.load(f)
+# X_train, X_val, y_train, y_val = train_test_split(data['features'], data['labels'], test_size=0.33, random_state=0)
 
-# Split data into training and validation sets.
-X_train, X_val, y_train, y_val = train_test_split(data['features'], data['labels'], test_size=0.33, random_state=0)
+# test on cifar-10
+from keras.datasets import cifar10
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+# y_train.shape is 2d, (50000, 1). While Keras is smart enough to handle this
+# it's a good idea to flatten the array.
+y_train = y_train.reshape(-1)
+y_test = y_test.reshape(-1)
+
+from sklearn.model_selection import train_test_split
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.3, random_state=42, stratify = y_train)
 
 
-# Define placeholders and resize operation.
-input_x = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
-input_y = tf.placeholder(tf.int64, shape=None)
-resized = tf.image.resize_images(input_x, [227, 227])
+features = tf.placeholder(tf.float32, (None, 32, 32, 3))
+labels = tf.placeholder(tf.int64, None)
+resized = tf.image.resize_images(features, (227, 227))
 
-
-# pass placeholder as first argument to `AlexNet`.
+# Returns the second final layer of the AlexNet model,
+# this allows us to redo the last layer for the traffic signs
+# model.
 fc7 = AlexNet(resized, feature_extract=True)
 fc7 = tf.stop_gradient(fc7)
-# dd the final layer for traffic sign classification.
-shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
-fc8W = tf.Variable(tf.truncated_normal(shape, mean=0.01))
-fc8b = tf.Variable(tf.truncated_normal([nb_classes]))
-out = tf.nn.xw_plus_b(fc7, fc8W, fc8b)
-probs = tf.nn.softmax(out)
+shape = (fc7.get_shape().as_list()[-1], nb_classes)
+fc8W = tf.Variable(tf.truncated_normal(shape, stddev=1e-2))
+fc8b = tf.Variable(tf.zeros(nb_classes))
+logits = tf.nn.xw_plus_b(fc7, fc8W, fc8b)
 
-
-# Define loss, training, accuracy operations.
-# HINT: Look back at your traffic signs project solution, you may
-# be able to reuse some the code.
-one_hot = tf.one_hot(input_y, nb_classes)
-
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot, logits=out)
+cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
 loss_op = tf.reduce_mean(cross_entropy)
 opt = tf.train.AdamOptimizer()
 train_op = opt.minimize(loss_op, var_list=[fc8W, fc8b])
 init_op = tf.global_variables_initializer()
-preds = tf.arg_max(out, 1)
-accuracy_op = tf.reduce_mean(tf.cast(tf.equal(preds, input_y), tf.float32))
+
+preds = tf.arg_max(logits, 1)
+accuracy_op = tf.reduce_mean(tf.cast(tf.equal(preds, labels), tf.float32))
 
 
-# Train and evaluate the feature extraction model.
-def eval(x, y, sess):
+def eval_on_data(X, y, sess):
     total_acc = 0
     total_loss = 0
-    for offset in range(0, x.shape[0], batch_size):
+    for offset in range(0, X.shape[0], batch_size):
         end = offset + batch_size
-        X_batch = x[offset:end]
+        X_batch = X[offset:end]
         y_batch = y[offset:end]
 
-        l, acc = sess.run([loss_op, accuracy_op], feed_dict={input_x: X_batch, input_y: y_batch})
-        total_loss += (l * X_batch.shape[0])
+        loss, acc = sess.run([loss_op, accuracy_op], feed_dict={features: X_batch, labels: y_batch})
+        total_loss += (loss * X_batch.shape[0])
         total_acc += (acc * X_batch.shape[0])
 
-    return total_loss / x.shape[0], total_acc / y.shape[0]
-
+    return total_loss/X.shape[0], total_acc/X.shape[0]
 
 with tf.Session() as sess:
     sess.run(init_op)
@@ -73,10 +73,10 @@ with tf.Session() as sess:
         t0 = time.time()
         for offset in range(0, X_train.shape[0], batch_size):
             end = offset + batch_size
-            sess.run(train_op, feed_dict={input_x: X_train[offset:end], input_y: y_train[offset:end]})
+            sess.run(train_op, feed_dict={features: X_train[offset:end], labels: y_train[offset:end]})
 
-        val_loss, val_acc = eval(X_val, y_val, sess)
-        print("Epoch", i + 1)
+        val_loss, val_acc = eval_on_data(X_val, y_val, sess)
+        print("Epoch", i+1)
         print("Time: %.3f seconds" % (time.time() - t0))
         print("Validation Loss =", val_loss)
         print("Validation Accuracy =", val_acc)
